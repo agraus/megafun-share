4202d8b452d459043e9b99e6527356d7091f4590
Agraus <agraus56@gmail.com>
2018-10-05 20:13:17 +0300

    added support for jpeg, bmp and gif previews

--- src/uploadhelper.php
+++ src/uploadhelper.php
@@ -10,11 +10,11 @@ abstract class UploadHelper
 		{
 			return "Name can't be longer than 180 characters";
 		}
-		if($file['type'] = 'image/png')
+		$new_name = random_int(1, 9999999999) .'.' .pathinfo($name, PATHINFO_EXTENSION);
+		if(strstr($file['type'], 'image'))
 		{
-			return self::makeFilePreview($tmp_name);
+			self::makeFilePreview($tmp_name, $file['type'],$new_name);
 		}
-		$new_name = random_int(1, 9999999) .'.' .pathinfo($name, PATHINFO_EXTENSION);
 		if(move_uploaded_file($tmp_name, $directory .$new_name))
 		{
 			return "File uploaded";
@@ -24,11 +24,18 @@ abstract class UploadHelper
 			return"Upload failed";
 		}
 	}
-	private static function makeFilePreview(string $file)
+	private static function makeFilePreview(string $file, string $type, string $filename)
 	{
 		$width = 800;
 		$height = 600;
-		list($width_orig, $height_orig) = getimagesize($file);
+		switch($type)
+		{
+    		case 'image/bmp': $image = imagecreatefromwbmp($file); break;
+    		case 'image/gif': $image = imagecreatefromgif($file); break;
+   		 	case 'image/jpeg': $image = imagecreatefromjpeg($file); break;
+   			case 'image/png': $image = imagecreatefrompng($file); break;
+ 		}
+ 		list($width_orig, $height_orig) = getimagesize($file);
 		$ratio_orig = $width_orig/$height_orig;
 		if ($width/$height > $ratio_orig) 
 		{
@@ -39,9 +46,19 @@ abstract class UploadHelper
    			$height = $width/$ratio_orig;
 		}
 		$image_preview = imagecreatetruecolor($width, $height);
-		$image = imagecreatefrompng($file);
+		if($type == "image/gif" or $type == "image/png")
+		{
+    		imagecolortransparent($image_preview, imagecolorallocatealpha($image_preview, 0, 0, 0, 127));
+    		imagealphablending($image_preview, false);
+    		imagesavealpha($image_preview, true);
+  		}
 		imagecopyresampled($image_preview, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
-		$image_preview = imagepng($image_preview, __DIR__ ."/../public/image.png", 0);
-		return $image_preview;
+		switch($type)
+		{
+    		case 'image/bmp': imagewbmp($image_preview, __DIR__ ."/../public/previews/" .$filename); break;
+    		case 'image/gif': imagegif($image_preview,  __DIR__ ."/../public/previews/" .$filename); break;
+    		case 'image/jpeg': imagejpeg($image_preview,  __DIR__ ."/../public/previews/" .$filename); break;
+    		case 'image/png': imagepng($image_preview,  __DIR__ ."/../public/previews/" .$filename); break;
+  		}
 	}
 }
